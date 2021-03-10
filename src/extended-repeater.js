743574ded6b130cf7a14b33de3324a3b2688d735
DariaShibalko <daryaavramo@yandex.by>
2021-03-10 23:52:49 +0300

    feat: done extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+  if(str=="") return false;
+  if(typeof(str)!='string') str=String(str);
+  if(typeof(options.addition)!='string') options.addition=String(options.addition);
+  if(typeof(options.repeatTimes)!='number' || options.repeatTimes%1!=0) options.repeatTimes=1
+  if(typeof(options.additionRepeatTimes)!='number' || options.additionRepeatTimes%1!=0 ) options.additionRepeatTimes=1;
+  if (options.separator==undefined) options.separator='+';
+  if (options.additionSeparator==undefined) options.additionSeparator="";
+
+  let newStr=[];
+  for(let i=0;i<options.repeatTimes;i++){
+    newStr.push(str);
+    
+    if(options.addition!="undefined"){
+
+      for(let j=0;j<options.additionRepeatTimes;j++){
+        newStr.push(options.addition);
+        newStr.push(options.additionSeparator);
+      }
+      newStr.pop();
+    }
+    
+    newStr.push(options.separator);
+  }
+  newStr.pop();
+  return newStr.join('');
 };
   
